75aa222aa4fde53bd91cf83087196737c2f7d19e
Shubham-xd <116097232+Shubham-xd@users.noreply.github.com>
2022-12-06 01:43:44 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -8,6 +8,10 @@ from plugins.web_support import web_server
 
 from plugins.cb_data import app as Client2
 
+logging.config.fileConfig('logging.conf')
+logging.getLogger().setLevel(logging.INFO)
+logging.getLogger("pyrogram").setLevel(logging.ERROR)
+
 TOKEN = os.environ.get("TOKEN", "")
 
 API_ID = int(os.environ.get("API_ID", ""))
